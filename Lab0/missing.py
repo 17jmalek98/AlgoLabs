1bcc6687198dfc60fc6616789b36bc07cbcf0da2
Joshua Malek <36347275+17jmalek98@users.noreply.github.com>
2019-10-04 09:07:43 -0400

    added explanation

--- Lab0/missing.py
+++ Lab0/missing.py
@@ -1,3 +1,5 @@
+#for a list of incrementing numbers 1 to x, find the missing number in the sequence.
+#uses the sum formula, (x * (x+1))/2 = sum of first x numbers
 arr = [6,5,4,1,2]
 maxi = 0
 total = 0
